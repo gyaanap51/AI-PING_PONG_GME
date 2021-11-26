f68616a8465fbae326bc16f3fd274bd762b8d1a9
gyaanap51 <85648046+gyaanap51@users.noreply.github.com>
2021-11-26 11:57:30 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function setup(){
+  var canvas =  createCanvas(700,600);
+  canvas.center();
+  
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+
+  }
+  
+function preload(){
+
+  }
